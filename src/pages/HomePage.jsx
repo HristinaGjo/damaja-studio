5e07566ce061c29b1cd4b026dd043884359bf11d
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-07-18 16:10:34 +0200

    scroll effect on hero Image and css fixes on projects container, grid and margin top

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,65 +1,75 @@
-import React from "react";
-import classes from "../styles/homePage.module.css"
+import React, { useRef } from "react";
+import classes from "../styles/homePage.module.css";
 import Navbar from "../components/Navbar";
 import heroImg from "../assets/heroImg.webp";
-import firstRowImg from "../assets/secondRowImg.webp"
-import secondRowImg from "../assets/firstRowImg.webp"
-import { useEffect, useRef } from "react";
+import firstRowImg from "../assets/secondRowImg.webp";
+import secondRowImg from "../assets/firstRowImg.webp";
 import { useScroll, useTransform, motion } from "framer-motion";
 
-
-
 const HomePage = () => {
-    
-    const pageCtn = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target:pageCtn,
-        offset:[ 'start end', 'end start' ]
-    })
+  const { scrollY } = useScroll();
+  const heroRef = useRef(null);
+  const projectsCtn = useRef(null);
 
-    const sm = useTransform (scrollYProgress, [0,1], [0,-150]);
-    const md = useTransform (scrollYProgress, [0,1], [0,-250]);
+  // Animate hero image to shrink and disappear as projectsCtn comes into view
+  /*const heroScale = useTransform(scrollY, [0, 300], [1, 0.8]);
+  const heroOpacity = useTransform(scrollY, [0, 300], [1, 0]);*/
 
-    return ( 
-        <>
+  const heroHeight = useTransform(scrollY, [0, 600], ['90vh', '50vh']);
+  
+  /*const heroOpacity = useTransform(scrollY, [0, 300], [1, 1]);*/
 
-        <div ref={pageCtn} className={classes.pageCtn}>
+  // Animate projectsCtn to parallax scroll
+  const { scrollYProgress } = useScroll({
+    target: projectsCtn,
+    offset: ['start end', 'end start'],
+  });
 
-            <div className={classes.heroCtn}>
-                <img src={heroImg} alt="Img not available" className={classes.heroImage}/>
+  const sm = useTransform(scrollYProgress, [0, 1], [0, -150]);
+  const md = useTransform(scrollYProgress, [0, 1], [0, -250]);
+
+  return (
+    <>
+      <div className={classes.pageCtn}>
+        <motion.div
+          ref={heroRef}
+          style={{ 
+            height: heroHeight,
+            /*transition: 'height 0.8s ease, opacity 0.8s ease-in-out'*/
+         }}
+          className={classes.heroCtn}
+        >
+          <img src={heroImg} alt="Img not available" className={classes.heroImage} />
+        </motion.div>
+
+        <div ref={projectsCtn} className={classes.projectsCtn}>
+          <motion.div style={{ y: sm }} className={classes.firstRow}>
+            <img src={firstRowImg} alt="Img not available" className={classes.firstRowImage} />
+            <div className={classes.projectName}>
+              <span>Adidas</span>
+              <span className={classes.spanMore}>more +</span>
             </div>
+          </motion.div>
 
-            <div className={classes.projectsCtn}>
-
-            <motion.div style={{y:sm}} className={classes.firstRow}>
-                <img src={firstRowImg} alt="Img not available" className={classes.firstRowImage}/>
-                <div className={classes.projectName}>
-                <span>Adidas</span>
-                <span className={classes.spanMore}>more +</span>
-                </div>
-            </motion.div>
-
-            <motion.div style={{y:sm}} className={classes.secondRow}>
-                <img src={secondRowImg} alt="Img not available" className={classes.secondRowImage}/>
-                <div className={classes.secondProjectName}>
-                <span>Adidas</span>
-                <span className={classes.spanMore}>more +</span>
-                </div>
-            </motion.div>
-
-            <motion.div style={{y:sm}} className={classes.thirdRow}>
-                <img src={firstRowImg} alt="Img not available" className={classes.thirdRowImage}/>
-                <div className={classes.projectName}>
-                <span>Adidas</span>
-                <span className={classes.spanMore}>more +</span>
-                </div>
-            </motion.div>
-
+          <motion.div style={{ y: sm }} className={classes.secondRow}>
+            <img src={secondRowImg} alt="Img not available" className={classes.secondRowImage} />
+            <div className={classes.secondProjectName}>
+              <span>Adidas</span>
+              <span className={classes.spanMore}>more +</span>
             </div>
+          </motion.div>
 
+          <motion.div style={{ y: sm }} className={classes.thirdRow}>
+            <img src={firstRowImg} alt="Img not available" className={classes.thirdRowImage} />
+            <div className={classes.thirdProjectName}>
+              <span>Adidas</span>
+              <span className={classes.spanMore}>more +</span>
+            </div>
+          </motion.div>
         </div>
-        </>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default HomePage;
